de4eeb058c2bd5e674391d24fc8b4440bd1614e8
Nemanja Apostolović <nemanja.apostolovic@sipodsoftware.com>
2021-07-24 00:31:56 +0200

    Build fix - Page 404 redirect

--- pages/404.js
+++ pages/404.js
@@ -2,7 +2,7 @@ export default function Custom404() {
     return <h1>404 - Page Not Found</h1>
 }
 
-export const getStaticProps = () => {
+export const getServerSideProps = () => {
     return {
       redirect: {
         destination: '/',
