264f724d22e1012836276054a27cd01680ba9757
Nemanja Apostolović <nemanja.apostolovic@sipodsoftware.com>
2021-07-24 01:33:34 +0200

    Update app URL

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production'
 
-export const server = dev ? 'http://localhost:3000' : 'https://www.mywebsite.com'
+export const server = dev ? 'http://localhost:3000' : 'https://world-in-2021.vercel.app/'
