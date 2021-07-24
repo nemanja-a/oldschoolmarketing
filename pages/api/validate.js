5b6adeb17e260d14a7bde68183129ed0a91ad543
Nemanja Apostolović <nemanja.apostolovic@sipodsoftware.com>
2021-07-24 18:43:31 +0200

    Google key file name path fixing

--- pages/api/validate.js
+++ pages/api/validate.js
@@ -24,7 +24,7 @@ export default async (req, res) => {
 
   // Case 3
   // Check if website has adult, medical, racy or any kind of disturbing content
-  const keyFilename = '../../famous-channels-f2d60f2dde10.json'
+  const keyFilename = '../../files/famous-channels-f2d60f2dde10.json'
   const projectId = 'famous-channels'
   const webRiskclient = new WebRiskServiceClient({projectId, keyFilename});
   const googleWebRiskRequest = {
