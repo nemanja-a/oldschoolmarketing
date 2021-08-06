773d2d928b8fb5a4baf10d5e7b92690d268d487c
Nemanja Apostolović <nemanja.apostolovic@sipodsoftware.com>
2021-08-06 19:29:15 +0200

    /validate fixing

--- pages/api/validate.js
+++ pages/api/validate.js
@@ -5,37 +5,37 @@ import { getGSACredentials, websiteExistInNearbyPages } from "../../lib/util"
 export default async (req, res) => {
 
   res.json({ok: true})
-  // const { db } = await connectToDatabase();
-  // const { url, page } = req.query
+  const { db } = await connectToDatabase();
+  const { url, page } = req.query
 
-  // // Case 1
-  // // Handle when website exist on one or more pages
-  // const websiteInDatabase = await db.collection("websites").find({"websites.url": url}).toArray()
-  // const matchFound = websiteInDatabase.length && websiteExistInNearbyPages(websiteInDatabase, Number(page))
-  // if (matchFound) return res.status(409).json({error: 'Website already exists'})
-  // // Case 1 end
+  // Case 1
+  // Handle when website exist on one or more pages
+  const websiteInDatabase = await db.collection("websites").find({"websites.url": url}).toArray()
+  const matchFound = websiteInDatabase.length && websiteExistInNearbyPages(websiteInDatabase, Number(page))
+  if (matchFound) return res.status(409).json({error: 'Website already exists'})
+  // Case 1 end
 
-  // // Case 2
-  // // Validate Domain Name Server
-  // const isUrlValid = await fetch(url, { method: 'HEAD' })
-  // if (!isUrlValid.status === 200) {
-  //   return res.status(404).json({error: `URL ${url} is not valid`})  
-  // }
-  // // Case 2 end
+  // Case 2
+  // Validate Domain Name Server
+  const isUrlValid = await fetch(url, { method: 'HEAD' })
+  if (!isUrlValid.status === 200) {
+    return res.status(404).json({error: `URL ${url} is not valid`})  
+  }
+  // Case 2 end
 
 
-  // // Case 3
-  // // Check if website has adult, medical, racy or any kind of disturbing content
-  // const projectId = process.env.GOOGLE_PROJECT_ID
-  // const credentials = getGSACredentials()
+  // Case 3
+  // Check if website has adult, medical, racy or any kind of disturbing content
+  const projectId = process.env.GOOGLE_PROJECT_ID
+  const credentials = getGSACredentials()
 
-  // const webRiskclient = new WebRiskServiceClient({projectId, credentials});
-  // const googleWebRiskRequest = {
-  //   uri: url,
-  //   threatTypes: ['MALWARE', 'SOCIAL_ENGINEERING', 'UNWANTED_SOFTWARE'],
-  // };
+  const webRiskclient = new WebRiskServiceClient({projectId, credentials});
+  const googleWebRiskRequest = {
+    uri: url,
+    threatTypes: ['MALWARE', 'SOCIAL_ENGINEERING', 'UNWANTED_SOFTWARE'],
+  };
 
-  // const {threat} = (await webRiskclient.searchUris(googleWebRiskRequest))[0];
-  // threat ? res.json({ok: false, msg: "The URL is marked as unsafe by Google Web Risk"}) : res.json({ok: true})
+  const {threat} = (await webRiskclient.searchUris(googleWebRiskRequest))[0];
+  threat ? res.json({ok: false, msg: "The URL is marked as unsafe by Google Web Risk"}) : res.json({ok: true})
   // Case 3 end
 }
