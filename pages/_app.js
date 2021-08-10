acff47749e2c54c47ea51cca9a6bc517f71d49e7
Nemanja Apostolović <nemanja.apostolovic@sipodsoftware.com>
2021-08-10 20:17:10 +0200

    Add device detection

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ function MyApp({ Component, pageProps }) {
   const isMobile = useDeviceDetect().isMobile
 
   return (
-      isMobile ? <Component {...pageProps} /> : <div style={
+      !isMobile ? <Component {...pageProps} /> : <div style={
         {background: "#cda500", display: "flex", justifyContent: "center",
          alignItems: "center", color: "white", fontSize: "10vh"}}
          >Coming Soon</div>
