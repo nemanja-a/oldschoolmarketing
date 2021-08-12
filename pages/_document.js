8ce79a6909ecf46ba1864c8e0fc4911b1b89ac6b
Nemanja Apostolović <nemanja.apostolovic@sipodsoftware.com>
2021-08-12 15:44:29 +0200

    Mobile header (WIP)

--- pages/_document.js
+++ pages/_document.js
@@ -6,8 +6,8 @@ class MyDocument extends Document {
       <Html>
         <Head>        
         <link href="https://fonts.googleapis.com/css2?family=Noto+Serif&display=swap" rel="stylesheet"></link>          
-          {/* <script defer src={`https://www.paypal.com/sdk/js?client-id=${process.env.PAYPAL_CLIENT_ID}`} />  */}
-          <script defer src={`https://www.paypal.com/sdk/js?client-id=sandbox`} />        
+          <script defer src={`https://www.paypal.com/sdk/js?client-id=${process.env.PAYPAL_CLIENT_ID}`} /> 
+          {/* <script defer src={`https://www.paypal.com/sdk/js?client-id=sandbox`} />         */}
         </Head>
         <body>
           <Main />
