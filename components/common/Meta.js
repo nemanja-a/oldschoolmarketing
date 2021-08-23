60f3dfb5b13912e25afa03c6ee9e4149f2a8f565
Nemanja Apostolović <nemanja.apostolovic@sipodsoftware.com>
2021-08-23 20:10:31 +0200

    Add portrait orientation lock

--- components/common/Meta.js
+++ components/common/Meta.js
@@ -7,6 +7,7 @@ const Meta = ({ title, keywords, description }) => {
             <meta name='keywords' content={keywords} />
             <meta name='description' content ={description} />
             <meta charSet='utf-8' />
+            <meta http-equiv="ScreenOrientation" content="autoRotate:disabled" />
             <title>{title}</title>
         </Head>
     )
