dbbf8862501a85d2bd5f1d820a56aa0cec2504c9
Nemanja Apostolović <nemanja.apostolovic@sipodsoftware.com>
2021-08-28 16:12:19 +0200

    Old School Marketing v1.0 release

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production'
 
-export const server = dev ? 'http://localhost:3000' : 'https://worldin2021.com'
+export const server = dev ? 'http://localhost:3000' : 'https://oldschoolmarketing.org'
